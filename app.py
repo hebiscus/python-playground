e520b88f7da72d0afd503ac2a285a7bbad35bb63
hebiscus <107350293+hebiscus@users.noreply.github.com>
2023-07-13 19:28:29 +0200

    inputs

---
+++ app.py
@@ -0,0 +1,13 @@
+print("important chinese proverb")
+
+name = "John Smith"
+age = 20
+isNew = True
+
+#inputs
+getName = input("What is your name? ")
+getFavColor = input("What's your favourite color? ")
+print(getName + " likes " + getFavColor)
+
+#strings
+
